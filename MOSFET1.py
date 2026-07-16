02013e9bcc2e56f3b64810ea0447060cd46c38bb
akshitguleria2009 <akshitguleria2009@gmail.com>
2026-07-16 08:05:22 +0530

    Updated MOSFET1 prototype 2.py
    Updated via refactoring input handling into reusable functions (16/06/2026

--- MOSFET1.py
+++ MOSFET1.py
@@ -1,45 +1,32 @@
 print('Welcome To MOSFET diagnostic Assistant')
-Dict = {"Dv" : input("Enter device name"),
-"VG" : input('Enter VGS'),
-"VD" : input('Enter VDS'),
-"Temp" : input('Enter Temperature'),
-"CL" : input('Enter Channel Length'),
-"CW" : input('Enter Channel Width'),
-}
-print(f"current status {Dict}")
-VG = float(Dict.get("VG"))
-VD = float(Dict.get("VD"))
-T = float(Dict.get("Temp"))
-CL = float(Dict.get("CL"))
-CW = float(Dict.get("CW"))
-n = 0
-while n < 1:
-     if VG < 0.7:
-        print('Device will not work')
-        print('Repair Cost= 150')
-        Dict ['VG'] = str(0.8)
-     else:
-        print('Your VGS is fine')
-     if T > 100.0:
-        print ('''Overheated
-        COOLING REQUIRED''')
-        Dict['Temp'] = 90
-     else:
-        print("Good going")
-     if VD > 5:
-        print('''High drain voltage''')
-        Dict['VD'] = 3
-     else :
-         print("DRAIN VOLTAGE IS FINE")
-     if CL < 20:
-         print("No requirement with length")
-     else :
-        print("Changes made free of cost")         
-        Dict['CL'] = 18
-     if CW < 15:
-         print("No requirement with width")
-     else :
-        print("Changes made")         
-        Dict['CL'] = 12       
-     n+= 1
-print(f"Changes made to the Conductor{Dict}")
+def data_inp (param):
+       value = float(input(f"Enter the {param} to be checked"))
+       return value
+def compare_less(name, value, threshold, repairv):
+       if value < threshold:
+           print(f"Checking {name}")
+           print(f"We optimized to {repairv}")
+           return repairv
+       else:
+           print("Checking successful no changes made")
+           print(f"Current value {value}")
+           return value
+def compare_more(name, value, threshold, repairv):
+       if value > threshold:
+           print(f"Checking {name}")
+           print(f"We optimized to {repairv}")
+           return repairv
+       else:
+           print("Checking successful no changes made")
+           print(f"Current value {value}")
+           return value
+VG = data_inp("VGS")
+VG = compare_less ("VGS value ", VG, 0.7, 0.8)
+VD = data_inp("VDS")
+VD = compare_more ("VDS value ", VD, 5.0, 3.0)
+TEMP = data_inp("Temperature ")
+TEMP = compare_more ("Temperature ", TEMP, 100.0, 90.0)
+CL = data_inp("Channel Length")
+CL = compare_more ("Channel Length ", CL, 20.0, 18.0)
+CW = data_inp("Channel Width")
+CW = compare_more ("Chennel Width ", CW, 15.0, 12.0)
