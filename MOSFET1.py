721dedc509b0c14cb92643b6a3f85cda142d4cbf
akshitguleria2009 <akshitguleria2009@gmail.com>
2026-07-23 10:01:55 +0530

    Update MOSFET1.py
    Prototype 3: Added MOSFET transfer characteristics simulator using NumPy and Matplotlib

--- MOSFET1.py
+++ MOSFET1.py
@@ -1,32 +1,62 @@
 print('Welcome To MOSFET diagnostic Assistant')
 def data_inp (param):
-       value = float(input(f"Enter the {param} to be checked"))
+       value = float(input(f"Enter the parameters= {param} "))
        return value
-def compare_less(name, value, threshold, repairv):
-       if value < threshold:
-           print(f"Checking {name}")
-           print(f"We optimized to {repairv}")
-           return repairv
-       else:
-           print("Checking successful no changes made")
-           print(f"Current value {value}")
-           return value
-def compare_more(name, value, threshold, repairv):
-       if value > threshold:
-           print(f"Checking {name}")
-           print(f"We optimized to {repairv}")
-           return repairv
-       else:
-           print("Checking successful no changes made")
-           print(f"Current value {value}")
-           return value
-VG = data_inp("VGS")
-VG = compare_less ("VGS value ", VG, 0.7, 0.8)
-VD = data_inp("VDS")
-VD = compare_more ("VDS value ", VD, 5.0, 3.0)
-TEMP = data_inp("Temperature ")
-TEMP = compare_more ("Temperature ", TEMP, 100.0, 90.0)
-CL = data_inp("Channel Length")
-CL = compare_more ("Channel Length ", CL, 20.0, 18.0)
-CW = data_inp("Channel Width")
-CW = compare_more ("Chennel Width ", CW, 15.0, 12.0)
+vgs = data_inp("Gate-Source Voltage (VGS)")
+vds = data_inp("Drain-Source Voltage (VDS)")
+vt = data_inp("Threshold Voltage (VT)")
+k = data_inp("Process Constant (k)")
+vov= vgs - vt
+print(f"Your current overdrive voltage {vov} V")
+if vgs < vt:
+    region = "Cutoff"
+
+elif vds < vov:
+    region = "Linear"
+
+else:
+    region = "Saturation"
+print(f"Operating Region: {region}")
+import numpy as np
+if region == "Cutoff":
+    ID = 0
+elif region == "Linear":
+    ID = k*(((vgs - vt)*vds) - ((vds)**2)/2)
+else:
+    ID = k*((vgs - vt)**2)/2
+vgs_sweep = np.linspace(0,5,500)
+print()
+
+print("---------MOSFET REPORT---------")
+
+print(f"Operating Region : {region}")
+
+print(f"Overdrive Voltage : {vov} V")
+
+print(f"Drain Current : {ID:.6f} A")
+
+print(f"Overdrive Voltage : {vov:.2f} V")
+id_sweep = np.zeros_like(vgs_sweep)
+for i, vg in enumerate(vgs_sweep):
+
+    vov = vg - vt
+
+    if vg < vt:
+        id_sweep[i] = 0
+
+    elif vds < vov:
+        id_sweep[i] = k * (((vg - vt) * vds) - ((vds**2)/2))
+
+    else:
+        id_sweep[i] = k * ((vg - vt)**2) / 2
+        
+import matplotlib.pyplot as plt
+plt.plot(vgs_sweep, id_sweep)
+plt.xlabel("VGS (V)")
+plt.ylabel("Drain Current (A)")
+plt.title("MOSFET Transfer Characteristics")
+plt.grid(True)
+plt.legend(["Transfer Curve"])
+plt.tight_layout()
+plt.savefig("transfer_curve.png")
+plt.show()
